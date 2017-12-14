703817519d5e1d656f8f60e57c3adbe482829b06
raawaa <wenjie_yu@yeah.net>
2017-12-14 23:48:25 +0800

    test run

---
+++ javbook.js
@@ -0,0 +1,6 @@
+var urls = document.querySelectorAll('span.content_bt_url');
+// for (elt of urls){
+//   elt.style.border="5px solid red";
+// }
+urls.forEach(ele=> {ele.style.border="5px solid red";});
+document.body.style.border="5px solid red";

d759af12b46edbe636ba3a28b60daae76d5b19ec
raawaa <wenjie_yu@yeah.net>
2017-12-17 10:26:19 +0800

    Add general site support
    update version number

--- src/background.js
+++ src/background.js
@@ -1,17 +1,38 @@
 browser.browserAction.onClicked.addListener(() => {
-    browser.tabs.query({
-        currentWindow: true,
-        active: true
-    }).then(sendMessage).catch(onError);
+    // let currentTab = getCurrentTab();
+    getCurrentTab().then(getHostUrlPattern).then(findRelatedTabs).then(sendCommand).catch(onError);
+
+
+
+    function getCurrentTab() {
+        return browser.tabs.query({
+            currentWindow: true,
+            active: true
+        }).then(t => t[0].url);
+    }
+
+    function getHostUrlPattern(url) {
+        let re = /.*:\/\/(.*?)\/.*/i;
+        let domainMatch = url.match(re)[1];
+        return "*://" + domainMatch + "/*";
+    }
+
+    function findRelatedTabs(hostUrlPattern) {
+        return browser.tabs.query({
+            url: hostUrlPattern
+        });
+    }
+
+
+    function sendCommand(tabs) {
+        for (let tab of tabs) {
+            browser.tabs.sendMessage(tab.id, {
+                command: "magnit"
+            });
+        }
+    }
+
+    function onError(error) {
+        console.error(`Error: ${error}`);
+    }
 });
-
-function sendMessage(tabs) {
-    // for (let tab of tabs) {
-    browser.tabs.sendMessage(tabs[0].id, { command: "magnit" });
-    // }
-}
-
-
-function onError(error) {
-    console.error(`Error: ${error}`);
-}

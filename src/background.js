daeac7e6e1b901efeed0b8ce71441d9278d64abb
raawaa <wenjie_yu@yeah.net>
2018-02-24 04:08:14 +0800

    working on more sites

--- src/background.js
+++ src/background.js
@@ -1,3 +1,15 @@
+import fp from "lodash/fp";
+import _ from "lodash";
+import { defaultSelectors } from "./defaults";
+
+browser.storage.sync.get('selectors')
+    .then(options => {
+        console.log(options);
+        if (_.isEmpty(options)) {
+            return browser.storage.sync.set({ selectors: defaultSelectors })
+        }
+    }).then();
+
 browser.browserAction.onClicked.addListener(() => {
     // let currentTab = getCurrentTab();
     getCurrentTab().then(getHostUrlPattern).then(findRelatedTabs).then(sendCommand).catch(onError);
@@ -25,14 +37,34 @@ browser.browserAction.onClicked.addListener(() => {
 
 
     function sendCommand(tabs) {
-        for (let tab of tabs) {
-            browser.tabs.sendMessage(tab.id, {
-                command: "magnit"
-            });
-        }
+        // for (let tab of tabs) {
+        //     browser.tabs.sendMessage(tab.id, {
+        //         command: "magnit"
+        //     });
+        // }
+        console.debug(tabs);
+        return fp.map(tab => {
+            return browser.tabs.executeScript(tab.id, { file: '/content.bundle.js' }).then((a) => console.debug('execute:', a)).catch(e => console.error(e));
+        })(tabs);
     }
 
     function onError(error) {
         console.error(`Error: ${error}`);
     }
 });
+
+
+// browser.runtime.onMessage.addListener(message => {
+//     console.debug(message);
+//     if (message.command == "regist") {
+//         console.debug(message.data);
+//         browser.contentScripts.register({
+//             matches: fp.map(entry => entry.url_pattern)(message.data),
+//             js: [
+//                 {
+//                     file: 'content.bundle.js'
+//                 }
+//             ]
+//         }).then();
+//     }
+// })
--- src/options.js
+++ src/options.js
@@ -1,19 +1,33 @@
 import _ from 'lodash';
+import { defaultSelectors } from './defaults';
 
 function saveOptions(e) {
-    browser.storage.sync.set({
-        selectors:JSON.parse(document.querySelector("#selectors").value)
-    });
+    let selectors = JSON.parse(document.querySelector("#selectors").value);
+    browser.storage.sync.set({ selectors })
+    .then(() => console.log(selectors));
     e.preventDefault();
 }
 
 function restoreOptions() {
-    let gettingItem = browser.storage.managed.get('selectors');
-    gettingItem.then(res=>{
-        document.querySelector("#selectors").value=res.selectors ||
-    })
+    let gettingItem = browser.storage.sync.get('selectors');
+    gettingItem.then(res => {
+        // console.debug(res);
+        document.querySelector("#selectors").value = JSON.stringify(res.selectors || defaultSelectors, null, 4)
+    });
+}
+
+function resetOptions(e) {
+    browser.storage.sync.set({
+        selectors: defaultSelectors
+    }).then(() => {
+        // console.debug('reseting');
+        document.querySelector('#selectors').value = JSON.stringify(defaultSelectors, null, 4);
+        // browser.runtime.sendMessage({ command: "regist", data: defaultSelectors });
+    });
+    e.preventDefault();
 }
 
 
 document.querySelector("form").addEventListener("submit", saveOptions);
 document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("reset", resetOptions);

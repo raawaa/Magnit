b89efb11738677f1dd740ee9ec86e84335071299
raawaa <wenjie_yu@yeah.net>
2018-02-23 22:48:03 +0800

    refactoring

--- src/content.js
+++ src/content.js
@@ -1,58 +1,50 @@
 import _ from 'lodash';
+import fp from 'lodash/fp';
 
-var urls = document.querySelectorAll('span.content_bt_url');
+// var urls = document.querySelectorAll('span.content_bt_url');
 // for (elt of urls){
 //   elt.style.border="5px solid red";
 // }
 // urls.forEach(ele => { ele.style.border = "5px solid red"; });
 // document.body.style.border = "5px solid red";
 
-function getNodeList(selector) {
-    return document.querySelectorAll(selector);
-}
-
-
-
-function getNodesAttr(nodeList, attrName) {
-    return Array.map(nodeList, x => x[attrName]);
-}
-
-function getMagnet() {
-    let nodeList = getNodeList('.content_bt_url>a');
-    return getNodesAttr(nodeList, 'href');
-}
-
-function getSizes() {
-    let sizeNodes = getNodeList('.dht_dl_size_content');
-    let sizeTexts = getNodesAttr(sizeNodes, 'textContent');
+function text2size(text) {
     let re = /([0-9\.]+)([GMTK]B)/i;
-    let sizes = _.map(sizeTexts, function (e) {
-        let found = e.match(re);
-        let num = found[1];
-        let unit
-        switch (found[2]) {
-            case 'GB':
-                unit = 1000;
-                break;
-            case 'MB':
-                unit = 1;
-            default:
-                unit = 0;
-                break;
-        }
-        return num * unit;
-    });
+    let found = text.match(re);
+    let num = found[1];
+    let unit;
+    switch (found[2]) {
+        case 'GB':
+            unit = 1000;
+            break;
+        case 'MB':
+            unit = 1;
+        default:
+            unit = 0;
+            break;
+    }
+    return num * unit;
+}
 
-    return sizes;
+function getMagnets(selector) {
+    let nodeList = document.querySelectorAll(selector);
+    return fp.map(ele=>ele['href'])(nodeList);
+}
+
+
+function getSizes(selector) {
+    let sizeNodes = getNodeList(selector);
+    let sizeTexts = fp.map(ele=>ele['textContent'])(sizeNodes);
+    return fp.map(text2size)(sizeTexts);
 }
 
 browser.runtime.onMessage.addListener(request => {
     if (request.command === "magnit") {
-        let magnetData = _.zip(getMagnet(), getSizes());
+        let magnetData = _.zip(getMagnets(magnetSelector), getSizes(sizeSelector));
         let sortedData = _.orderBy(magnetData, 1, 'desc');
-        console.log(sortedData);
-        console.log("ROGAR THAT!!!!!");
-        window.location.href=sortedData[0][0];
+        // console.log(sortedData);
+        // console.log("ROGAR THAT!!!!!");
+        window.location.href = sortedData[0][0];
     }
 })
 
--- src/options.js
+++ src/options.js
@@ -1,4 +1,19 @@
 import _ from 'lodash';
 
+function saveOptions(e) {
+    browser.storage.sync.set({
+        selectors:JSON.parse(document.querySelector("#selectors").value)
+    });
+    e.preventDefault();
+}
+
+function restoreOptions() {
+    let gettingItem = browser.storage.managed.get('selectors');
+    gettingItem.then(res=>{
+        document.querySelector("#selectors").value=res.selectors ||
+    })
+}
+
+
 document.querySelector("form").addEventListener("submit", saveOptions);
 document.addEventListener('DOMContentLoaded', restoreOptions);
